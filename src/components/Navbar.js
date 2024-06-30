d735be7b87c07fb65af2ded431391a5e6ea4eece
dtbrexy <139590082+dtbrexy@users.noreply.github.com>
2024-06-30 20:21:14 +0200

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
           <ul>
             <li>
               <Link className="h-color" href="/">
-                BOT
+                BANE
               </Link>
             </li>
           </ul>
