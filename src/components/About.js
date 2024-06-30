b2af6ea8c40809241ea43364322b3564666e89e8
dtbrexy <139590082+dtbrexy@users.noreply.github.com>
2024-06-30 19:24:11 +0200

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -5,29 +5,28 @@ const About = () => {
     <>
       <section className="about-section" id="about-section">
         <div className="about-header">
-          <h3 className="h-color">Why use this bot?</h3>
+          <h3 className="h-color">Why bane?</h3>
         </div>
         <div className="about-container">
           <div className="about-card">
             <div>
-              <h4 className="h-color">⚡ Commands</h4>
+              <h4 className="h-color">Commands</h4>
               <p className="p-color">
-                We have more than 200 commands support in this bot.
+                Bane offers 600 commands.
               </p>
               <hr />
             </div>
             <div>
-              <h4 className="h-color">✉️ 24/7 Support</h4>
+              <h4 className="h-color">✉️ Support</h4>
               <p className="p-color">
-                The biggest support community server for the bot available for
-                you anytime.
+                our support is not active yet.
               </p>
               <hr />
             </div>
             <div>
-              <h4 className="h-color">✏️ Customization</h4>
+              <h4 className="h-color">🌎 Customization</h4>
               <p className="p-color">
-                Fully customizable discord bot completely FREE!
+                An all-in-one bot, user-focused commands, and is completely free!
               </p>
               <hr />
             </div>
@@ -41,15 +40,15 @@ const About = () => {
 
         <section className="about-stats">
           <div className="about-stats-card">
-            <h4 className="h-color">17k+</h4>
-            <p className="p-color">Total Servers</p>
+            <h4 className="h-color">20+</h4>
+            <p className="p-color">Total Guilds</p>
           </div>
           <div className="about-stats-card">
-            <h4 className="h-color">94k+</h4>
+            <h4 className="h-color">2k+</h4>
             <p className="p-color">Total Users</p>
           </div>
           <div className="about-stats-card">
-            <h4 className="h-color">375k+</h4>
+            <h4 className="h-color">10k+</h4>
             <p className="p-color">Total Commands</p>
           </div>
         </section>
