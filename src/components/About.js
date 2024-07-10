03e978d224c217df36b6cf18d64d08a44d6529b3
phobu <156504941+phobu@users.noreply.github.com>
2024-07-10 15:26:54 -0400

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -5,28 +5,28 @@ const About = () => {
     <>
       <section className="about-section" id="about-section">
         <div className="about-header">
-          <h3 className="h-color">Why bane?</h3>
+          <h3 className="h-color">Why glory?</h3>
         </div>
         <div className="about-container">
           <div className="about-card">
             <div>
               <h4 className="h-color">Commands</h4>
               <p className="p-color">
-                Bane offers 226 commands.
+                Glory offers 226 commands.
               </p>
               <hr />
             </div>
             <div>
               <h4 className="h-color">✉️ Support</h4>
               <p className="p-color">
-                our support is not active yet.
+                Support @ /undo.
               </p>
               <hr />
             </div>
             <div>
               <h4 className="h-color">🌎 Customization</h4>
               <p className="p-color">
-                An all-in-one bot, user-focused commands.
+                An all-in-one, multi-purpose, discord bot. .
               </p>
               <hr />
             </div>
@@ -40,15 +40,15 @@ const About = () => {
 
         <section className="about-stats">
           <div className="about-stats-card">
-            <h4 className="h-color">20+</h4>
+            <h4 className="h-color">17+</h4>
             <p className="p-color">Total Guilds</p>
           </div>
           <div className="about-stats-card">
-            <h4 className="h-color">200</h4>
+            <h4 className="h-color">4663</h4>
             <p className="p-color">Total Users</p>
           </div>
           <div className="about-stats-card">
-            <h4 className="h-color">10k+</h4>
+            <h4 className="h-color">46k+</h4>
             <p className="p-color">Total used Commands</p>
           </div>
         </section>
