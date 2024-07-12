84b6e8b0f0dfe3395e59aeea97a3e380b9f1ce4b
phobu <156504941+phobu@users.noreply.github.com>
2024-07-12 01:12:30 -0400

    Update commands.js

--- src/pages/commands.js
+++ src/pages/commands.js
@@ -62,16 +62,16 @@ export default function Commands() {
             <div className="box-content p-color">
               <ul>
                 <li>
-                  <kbd>!duckduckgo</kbd> -{" "}
-                  <span className="p-color">Search the DuckDuckGo search engine.</span>
+                  <kbd>$fact</kbd> -{" "}
+                  <span className="p-color">generate fact commands.</span>
                 </li>
                 <li>
-                  <kbd>!ttt</kbd> -{" "}
+                  <kbd>$ttt</kbd> -{" "}
                   <span className="p-color">Play tic-tac-toe with someone.</span>
                 </li>
                 <li>
-                  <kbd>!anime</kbd> -{" "}
-                  <span className="p-color">Search MyAnimeList for anime information.</span>
+                  <kbd>$yes or no</kbd> -{" "}
+                  <span className="p-color">ask a bot a question for a 50/50 response of Yes and No.</span>
                 </li>
               </ul>
             </div>
@@ -85,16 +85,16 @@ export default function Commands() {
             <div className="box-content p-color">
               <ul>
                 <li>
-                  <kbd>!react</kbd> -{" "}
-                  <span className="p-color">Play a game of reaction time</span>
+                  <kbd>$rps</kbd> -{" "}
+                  <span className="p-color">rock paper scissors someone</span>
                 </li>
                 <li>
-                  <kbd>!snake</kbd> -{" "}
+                  <kbd>$snake</kbd> -{" "}
                   <span className="p-color">Play a game of snake</span>
                 </li>
                 <li>
-                  <kbd>!memory</kbd> -{" "}
-                  <span className="p-color">Play a game of memory.</span>
+                  <kbd>worlde</kbd> -{" "}
+                  <span className="p-color">Play a game of wordle.</span>
                 </li>
               </ul>
             </div>
@@ -103,22 +103,22 @@ export default function Commands() {
           <input type="radio" name="accordion" id="cb4" />
           <section className="box">
             <label className="box-title p-color" htmlFor="cb4">
-              Configuration Commands
+              Moderation Commands
             </label>
             <label className="box-close" htmlFor="acc-close"></label>
             <div className="box-content p-color">
               <ul>
                 <li>
-                  <kbd>!wlc-set</kbd> -{" "}
-                  <span className="p-color">welcome system.</span>
+                  <kbd>$ban</kbd> -{" "}
+                  <span className="p-color">ban a user</span>
                 </li>
                 <li>
-                  <kbd>!marry, !divorce, !partner</kbd> -{" "}
-                  <span className="p-color">Marriage system.</span>
+                  <kbd>$unban</kbd> -{" "}
+                  <span className="p-color">unban a user</span>
                 </li>
                 <li>
-                  <kbd>!antilink</kbd> -{" "}
-                  <span className="p-color">antilink system.</span>
+                  <kbd>$kick</kbd> -{" "}
+                  <span className="p-color">$kick a user</span>
                 </li>
               </ul>
             </div>
@@ -133,40 +133,16 @@ export default function Commands() {
             <div className="box-content p-color">
               <ul>
                 <li>
-                  <kbd>!hentai</kbd> -{" "}
-                  <span className="p-color">hentai content.</span>
+                  <kbd>$nsfw bj</kbd> -{" "}
+                  <span className="p-color">blowjobs.</span>
                 </li>
                 <li>
-                  <kbd>!nsfw</kbd> -{" "}
-                  <span className="p-color">nsfw images.</span>
+                  <kbd>$nsfw waifu</kbd> -{" "}
+                  <span className="p-color">waifus.</span>
                 </li>
                 <li>
-                  <kbd>!ass</kbd> -{" "}
-                  <span className="p-color">ass images.</span>
-                </li>
-              </ul>
-            </div>
-          </section>
-
-                      <input type="radio" name="accordion" id="cb5" />
-          <section className="box">
-            <label className="box-title p-color" htmlFor="cb5">
-              Moderation Commands
-            </label>
-            <label className="box-close" htmlFor="acc-close"></label>
-            <div className="box-content p-color">
-              <ul>
-                <li>
-                  <kbd>$ban</kbd> -{" "}
-                  <span className="p-color">ban someone.</span>
-                </li>
-                <li>
-                  <kbd>$kick</kbd> -{" "}
-                  <span className="p-color">kick someone.</span>
-                </li>
-                <li>
-                  <kbd>$unban</kbd> -{" "}
-                  <span className="p-color">unban someone.</span>
+                  <kbd>$nsfw fj</kbd> -{" "}
+                  <span className="p-color">footjobs.</span>
                 </li>
               </ul>
             </div>
